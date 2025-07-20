64c87ebca2f9d8a13750b53cf135ab14675b07c4
Sai <SaiCodes007@users.noreply.github.com>
2025-07-20 09:42:57 +0530

    JWT

--- week-6-authentication/index.js
+++ week-6-authentication/index.js
@@ -1,83 +1,10 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "randomsai"
 const app = express();
 app.use(express.json());
 
-const users = [];
-
-function generateToken() {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    // use a simple function here
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-}
+const users = []
 
 app.post("/signup", function(req, res){
     const username = req.body.username;
@@ -106,8 +33,10 @@ app.post("/signin", function(req, res){
     }
 
     if (founduser){
-        const token = generateToken();
-        founduser.token = token;
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET);
+        // founduser.token = token;
 
         res.json({
             token: token
@@ -122,10 +51,13 @@ app.post("/signin", function(req, res){
 
 app.get("/me", function(req,res){
     const token = req.headers.token;
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+    const username = decodedInfo.username
+    
     let founduser = null;
 
     for (let i=0; i<users.length; i++){
-        if (users[i].token == token){
+        if (users[i].username == username){
             founduser = users[i]  
         }
     }
